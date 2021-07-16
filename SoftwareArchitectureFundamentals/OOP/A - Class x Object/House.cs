b1f73bbc862aaaca0462c690009d110760f3dbf6
Willian Chicanoski <willianchicanoski@outlook.com>
2021-07-16 17:57:30 -0300

    Add exemple of Class and Object

---
+++ SoftwareArchitectureFundamentals/OOP/A - Class x Object/House.cs
@@ -0,0 +1,29 @@
+﻿namespace OOP
+{
+    //Class definition
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Nacionalidade { get; set; }
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+
+    }
+
+    public class Object
+    {
+        public Object()
+        {
+            //Object definition
+            var pessoa = new Pessoa
+            {
+                Nome = "Willian Chicanoski",
+                Idade = 27,
+                Nacionalidade = "Brasileiro",
+                Altura = 1.76,
+                Peso = 90.0
+            };
+        }
+    }
+}
